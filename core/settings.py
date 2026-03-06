339d9c1d392ae2af50940758da82a6fd2cd3d5fb
Nurzhan <12farit21@gmail.com>
2026-03-06 23:16:47 +0500

    swithc to postgresql

--- core/settings.py
+++ core/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'lyn_furniture'),
+        'USER': os.environ.get('DB_USER', 'lyn_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
